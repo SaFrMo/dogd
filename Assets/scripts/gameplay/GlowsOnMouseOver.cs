8db638491a889ebb80f6d0a5fd48f1bac596706a
Sander <sandermoolin@gmail.com>
2014-02-19 14:06:51 -0500

    Mouseover glow script

---
+++ Assets/scripts/gameplay/GlowsOnMouseOver.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlowsOnMouseOver : MonoBehaviour {
+
+	// IMPLEMENTATION:
+	// =================
+	// Base class for anything that glows on mouseover.
+	// Requires Silhouetted Diffuse shader to work.
+
+	public Color outlineColor = new Color (0, 255, 0);
+	public float outlineWidth = 0.01f;
+	
+	public Shader glow;
+	
+	Shader originalShader;
+	
+	protected void Start () {
+		originalShader = renderer.material.shader;
+	}
+	
+	protected void OnMouseEnter () {
+		renderer.material.shader = glow;
+		renderer.material.SetColor ("_OutlineColor", outlineColor);
+		renderer.material.SetFloat ("_Outline", outlineWidth);
+	}
+
+	protected void OnMouseExit () {
+		renderer.material.shader = originalShader;
+	}
+}
---
+++ Assets/scripts/gameplay/computer terminals/insideA_term1.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class insideA_term1 : GlowsOnMouseOver {
+
+	bool showScreen = false;
+
+	void OnMouseOver() {
+		if (Input.GetMouseButtonDown(0)) {
+			showScreen = true;
+		}
+	}
+}
