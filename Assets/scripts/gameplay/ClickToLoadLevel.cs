b3729fefbff61dd8fba2e33cd763859e57aa35e8
Sander <sandermoolin@gmail.com>
2014-02-18 21:07:03 -0500

    Better lighting = better game

---
+++ Assets/scripts/gameplay/ClickToLoadLevel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToLoadLevel : MonoBehaviour {
+
+	public string levelToLoad;
+
+	public Color outlineColor = new Color (0, 255, 0);
+	public float outlineWidth = 0.01f;
+
+	public Shader glow;
+
+	Shader originalShader;
+
+	void Start () {
+		originalShader = renderer.material.shader;
+	}
+
+	void OnMouseEnter () {
+		renderer.material.shader = glow;
+		renderer.material.SetColor ("_OutlineColor", outlineColor);
+		renderer.material.SetFloat ("_Outline", outlineWidth);
+	}
+
+	void OnMouseOver () {
+		if (Input.GetMouseButtonDown (0)) {
+			Application.LoadLevel (levelToLoad);
+		}
+	}
+
+	void OnMouseExit () {
+		renderer.material.shader = originalShader;
+	}
+}
---
+++ Assets/scripts/lights/FlashingLight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashingLight : MonoBehaviour {
+
+	public float flashRate;
+
+	float originalIntensity;
+
+	Timer t;
+
+	void Start () {
+		t = new Timer (flashRate, true);
+		originalIntensity = light.intensity;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (t.RunTimer()) {
+			if (light.intensity == originalIntensity) {
+				light.intensity = 0;
+			}
+			else {
+				light.intensity = originalIntensity;
+			}
+		}
+	}
+}
