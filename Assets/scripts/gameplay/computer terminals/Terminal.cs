bb24bb99be947bab228dfd94f2bcbff32a3707d5
Sander <sandermoolin@gmail.com>
2014-02-24 21:08:07 -0500

    Script work started

---
+++ Assets/scripts/gameplay/computer terminals/Terminal.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Terminal : GlowsOnMouseOver {
+
+	bool showScreen = false;
+
+	void OnMouseOver() {
+		if (Input.GetMouseButtonDown(0)) {
+			showScreen = true;
+		}
+	}
+}
