9bff6f2945d98167e640d224da6e609f79ac76eb
Sander <sandermoolin@gmail.com>
2014-03-02 18:54:40 -0500

    Started vague commercial work

---
+++ Assets/scripts/_commercial/Desert_Commercial.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Desert_Commercial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
